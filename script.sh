f5fbd94cc71d8d0b0971d40389b660c50382597a
Jeffry <57407624+jmeneses22@users.noreply.github.com>
2020-02-20 12:53:46 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -14,7 +14,6 @@ apt install fish
 echo installed fish
 apt install w3m
 echo installed w3m
-apt
 apt install sl 
 sl
 apt install cmatrix
@@ -43,6 +42,7 @@ apt install ruby -y
 echo installed ruby
 figlet ruby2
 apt install ruby2
+echo installed ruby2
 figlet java
 apt install java
 echo installed java 
@@ -51,14 +51,19 @@ echo  necesary packs installed
 "###################" 
 figlet red_hawk
 git clone https://github.com/Tuhinshubhra/RED_HAWK.git
+echo cloned red_hawk.git 
 figlet credmap
 git clone https://github.com/lightos/credmap.git
+echo cloned credmap.git 
 figlet sqlmap
 git clone https://github.com/sqlmapproject/sqlmap.git
+echo cloned sqlmap.git 
 figlet recondog 
 git clone https://github.com/UltimateHackers/ReconDog.git
+echo cloned recondog.git
 figlet ngrok 
 git clone https://github.com/inconshreveable/ngrok.git
+echo cloned ngrok.git
 figlet tor
 apt install tor
 echo installed tor
@@ -70,6 +75,7 @@ apt install w3m
 echo instalando w3m
 figlet apache2
 apt install apache2
+echo installed apache2
 figlet cowsay
 apt install cowsay
 cowsay type-cmatrix
