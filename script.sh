ea1c4d9a4897fdabdf518e0c1ebd3b87501edf51
Jeffry <57407624+jmeneses22@users.noreply.github.com>
2020-02-20 09:29:48 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,80 @@
 "###############################"
- echo Welcome-Hackers
+ echo Welcome Hackers
 "###############################"
+apt install figlet 
+figlet NoHack|NoFun
+figlet update
+apt-get update
+figlet upgrade
+apt-get upgrade -y
+echo sucesfull terminal update
+apt install cmatrix
+echo installed cmatrix
+apt install fish
+echo installed fish
+apt install w3m
+echo installed w3m
+apt
+apt install sl 
+sl
+apt install cmatrix
+"####################"
+figlet nmap 
+"####################"
+figlet nmap
+"#################"
+echo installing nmap
+apt install nmap -y
+echo installed nmap
+figlet python
+apt install python -y
+echo installed python
+figlet php
+apt install php
+echo installed php 
+figlet python2
+apt install python2
+echo installed python2
+figlet python3
+apt install python3
+echo installed python3
+figlet ruby
+apt install ruby -y
+echo installed ruby
+figlet ruby2
+apt install ruby2
+figlet java
+apt install java
+echo installed java 
+"###################"
+echo  necesary packs installed
+"###################" 
+figlet red_hawk
+git clone https://github.com/Tuhinshubhra/RED_HAWK.git
+figlet credmap
+git clone https://github.com/lightos/credmap.git
+figlet sqlmap
+git clone https://github.com/sqlmapproject/sqlmap.git
+figlet recondog 
+git clone https://github.com/UltimateHackers/ReconDog.git
+figlet ngrok 
+git clone https://github.com/inconshreveable/ngrok.git
+figlet tor
+apt install tor
+echo installed tor
+figlet perl
+apt install perl 
+echo installed perl
+figlet w3m
+apt install w3m 
+echo instalando w3m
+figlet apache2
+apt install apache2
+figlet cowsay
+apt install cowsay
+cowsay type-cmatrix
+apt-get install cmatrix
+figlet NOHACK|NOFUN
+"###########################"
+figlet "Happy Hacking"
+"###########################"
