5974d8b8c7e189052a2e6bad1dbc8f198cd7ede2
Alonso <57407624+xeroproject@users.noreply.github.com>
2021-05-17 09:22:29 -0600

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,11 @@
+"###############################"
+ figlet Update and Upgrade...  
+"###############################"
+figlet -f banner update
+apt-get update
+figlet -f big upgrade
+apt-get upgrade -y
+echo sucesfull terminal update 
+"###########################"
+figlet "NO H4CK|NO FUN"
+"###########################"
